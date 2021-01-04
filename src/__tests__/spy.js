bf0aa23795a149f8a6fe080070723f918304eeed
Andrea Cigana <ciganandrea@gmail.com>
2021-01-04 21:39:34 +0100

    Add jest.spyOn()

--- src/__tests__/spy.js
+++ src/__tests__/spy.js
@@ -1,17 +1,17 @@
-const thumbWar = require('../thumb-war')
-const utils = require('../utils')
+const utils = require('../utils');
+const thumbWar = require('../thumb-war');
 
 test('returns winner', () => {
-  jest.spyOn(utils, 'getWinner')
-  utils.getWinner.mockImplementation((p1, p2) => p1)
+	jest.spyOn(utils, 'getWinner');
+	utils.getWinner.mockImplementation((player1, player2) => player1);
 
-  const winner = thumbWar('Kent C. Dodds', 'Ken Wheeler')
-  expect(winner).toBe('Kent C. Dodds')
-  expect(utils.getWinner.mock.calls).toEqual([
-    ['Kent C. Dodds', 'Ken Wheeler'],
-    ['Kent C. Dodds', 'Ken Wheeler']
-  ])
+	const winner = thumbWar('Super Mario', 'Superman');
 
-  // cleanup
-  utils.getWinner.mockRestore()
-})
+	expect(winner).toBe('Super Mario');
+	expect(utils.getWinner.mock.calls).toEqual([
+		['Super Mario', 'Superman'],
+		['Super Mario', 'Superman'],
+	]);
+
+	utils.getWinner.mockRestore();
+});
--- src/no-framework/spy.js
+++ src/no-framework/spy.js
@@ -1,32 +1,39 @@
-const assert = require('assert')
-const thumbWar = require('../thumb-war')
-const utils = require('../utils')
+const assert = require('assert');
 
-function fn(impl = () => {}) {
-  const mockFn = (...args) => {
-    mockFn.mock.calls.push(args)
-    return impl(...args)
-  }
-  mockFn.mock = {calls: []}
-  mockFn.mockImplementation = newImpl => (impl = newImpl)
-  return mockFn
+const utils = require('../utils');
+const thumbWar = require('../thumb-war');
+
+function fn(implementation = () => {}) {
+	const mockFn = (...args) => {
+		mockFn.mock.calls.push(args);
+		return implementation(...args);
+	};
+
+	mockFn.mock = { calls: [] };
+	mockFn.mockImplementation = (newImplementation) => {
+		implementation = newImplementation;
+	};
+
+	return mockFn;
 }
 
-function spyOn(obj, prop) {
-  const originalValue = obj[prop]
-  obj[prop] = fn()
-  obj[prop].mockRestore = () => (obj[prop] = originalValue)
+function spyOn(obj, key) {
+	const originalValue = obj[key];
+
+	obj[key] = fn();
+	obj[key].mockRestore = () => (obj[key] = originalValue);
 }
 
-spyOn(utils, 'getWinner')
-utils.getWinner.mockImplementation((p1, p2) => p1)
+spyOn(utils, 'getWinner');
 
-const winner = thumbWar('Kent C. Dodds', 'Ken Wheeler')
-assert.strictEqual(winner, 'Kent C. Dodds')
+utils.getWinner.mockImplementation((player1, player2) => player1);
+
+const winner = thumbWar('Super Mario', 'Superman');
+
+assert.strictEqual(winner, 'Super Mario');
 assert.deepStrictEqual(utils.getWinner.mock.calls, [
-  ['Kent C. Dodds', 'Ken Wheeler'],
-  ['Kent C. Dodds', 'Ken Wheeler']
-])
+	['Super Mario', 'Superman'],
+	['Super Mario', 'Superman'],
+]);
 
-// cleanup
-utils.getWinner.mockRestore()
+utils.getWinner.mockRestore();
